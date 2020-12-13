fb2a0a9ef3c07d81e69b3a07d774aade2b256238
wbrink <will.brink55@gmail.com>
2020-12-12 22:15:02 -0800

    fixed bug where the song in seconds would display instead of mm:ss on SongItem when artistSearch was done

--- client/src/Components/SongItem/SongItem.js
+++ client/src/Components/SongItem/SongItem.js
@@ -49,7 +49,7 @@ const SongItem = (props) => {
         <div>{props.plays}</div>
         
         <div className={styles.durationAndAction}>
-          <div>{props.duration}</div>
+          <div>{getFormattedSongDuration(props.duration)}</div>
           <div className={styles.callToAction}>
             <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
