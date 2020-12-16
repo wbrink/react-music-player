e8ab842111b15a7b32cb7d44f329c541cb20a01b
wbrink <will.brink55@gmail.com>
2020-12-15 23:24:19 -0800

    fixed mainContainer grid area so that overflow is auto and doesn't push down any content

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -198,6 +198,19 @@ const Sidebar = (props) => {
         </div>
 
         <div className={styles.menu} style={showMenu ? {opacity: "1", left: "0"} : {opacity: "0", left: "-100%"}}>
+          <div className={styles.search}>
+            <svg viewBox="0 0 16 16" className={styles.searchIcon} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+              <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+            </svg>
+            <input type="text" id="search" placeholder="Search" value={search} onChange={handleSearch} maxLength={40} ref={inputRef}/>
+
+            {/* close icon when text appears*/}
+            {search && <svg onClick={e => setSearch("")} width="1em" height="1em" viewBox="0 0 16 16" className={styles.clearTextButton} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+            </svg>}
+          </div>
+
           <div className={styles.links}>
             <NavLink to="/library" activeClassName={styles.active}>Library</NavLink>
             <NavLink to="/explore" activeClassName={styles.active}>Explore</NavLink>
